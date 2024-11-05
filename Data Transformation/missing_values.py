09dbceca209ffc673b6e0acf16812007f83ddce7
dv11079 <denisa-iulia.vaidasigan@stud.th-deg.de>
2024-11-05 10:27:53 +0100

    created jupyter notebook for data analysis and transformation

--- Data Transformation/missing_values.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('cleaned_dataset.csv')
-df.dropna()
-df = df.drop_duplicates()
-df.reset_index(drop=True, inplace=True)
-
-df.to_csv('cleaned_dataset.csv', index=False)
